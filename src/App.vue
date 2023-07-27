cb5f2565ecd38fe2c00d8549dc069cfe4ea94e02
unknown <cindyauliani0629@gmail.com>
2023-07-27 11:01:42 -0300

    tugastailwind

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 </template>
 <script setup>
 import Navbar from './components/Navbar.vue';
-import Avatar from './components/Avatar.vue'
+import Avatar from './components/Avatar.vue';
 import Flowbite from './components/Flowbite.vue';
 import Pictures from './components/Pictures.vue';
 import Footer from './components/Footer.vue';
