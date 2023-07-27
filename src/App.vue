d281b484afefd7e6c3ebe0420e692deed9981740
unknown <cindyauliani0629@gmail.com>
2023-07-27 10:41:16 -0300

    tugas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Navbar></Navbar>
-    <avatar-vue></avatar-vue>
+    <Avatar></Avatar>
     <Flowbite></Flowbite>
     <Pictures></Pictures>
     <Footer></Footer>
@@ -10,10 +10,9 @@
 </template>
 <script setup>
 import Navbar from './components/Navbar.vue';
-import avatarVue from './components/avatar.vue';
-import Flowbite from './components/flowbite.vue';
-import Pictures from './components/pictures.vue';
-import Footer from './components/footer.vue';
-
+import Avatar from './components/Avatar.vue'
+import Flowbite from './components/Flowbite.vue';
+import Pictures from './components/Pictures.vue';
+import Footer from './components/Footer.vue';
 
 </script>
